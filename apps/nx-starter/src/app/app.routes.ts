e78f5f81e6207c5c63379c91d1d2ba3bae72201e
artesgo <artesgo@gmail.com>
2024-12-17 15:37:00 -0500

    generate lazy loaded route and home component

--- apps/nx-starter/src/app/app.routes.ts
+++ apps/nx-starter/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Route[] = [
   {
-    path: '', redirectTo: 'daisy', pathMatch: 'full',
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'calendar', loadChildren: () => import('@nx-starter/calendar').then(m => m.calendarRoutes)
   }
 ];
