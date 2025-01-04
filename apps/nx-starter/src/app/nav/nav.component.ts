3f7ffb83f581e984a9441b461f9c6f2a9e71a14c
artesgo <artesgo@gmail.com>
2025-01-04 16:55:55 -0500

    control flow @if and ngIf

--- apps/nx-starter/src/app/nav/nav.component.ts
+++ apps/nx-starter/src/app/nav/nav.component.ts
@@ -1,15 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ThemeComponent } from 'ng-daisy';
 import { Button } from 'primeng/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [CommonModule, Button, ThemeComponent, RouterLink],
+  imports: [Button, ThemeComponent, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
 export class NavComponent {
   theme = signal('cupcake');
+  loggedIn = signal(false);
+
+  login() {
+    this.loggedIn.set(true);
+  }
+
+  logout() {
+    this.loggedIn.set(false);
+  }
 }
