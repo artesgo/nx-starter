e478584af940ca8da1c061363ad59c4f32031015
artesgo <artesgo@gmail.com>
2025-01-12 16:02:58 -0500

    @for and ngFor

--- apps/nx-starter/src/app/home/home.component.ts
+++ apps/nx-starter/src/app/home/home.component.ts
@@ -1,7 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  imports: [RouterLink, CommonModule],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  menu = [
+    {
+      label: 'Link 1',
+      value: 'link-1',
+    },
+    {
+      label: 'Link 2',
+      value: 'link-2',
+    },
+    {
+      label: 'Link 3',
+      value: 'link-3',
+    },
+    {
+      label: 'Link 4',
+      value: 'link-4',
+    },
+    {
+      label: 'Link 5',
+      value: 'link-5',
+    },
+  ];
+}
