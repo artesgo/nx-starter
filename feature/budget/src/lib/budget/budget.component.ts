4f60f7d330fd4c197f5a700c06236b2de362e7c7
artesgo <artesgo@gmail.com>
2025-06-21 23:58:31 -0400

    budgetting fn

--- feature/budget/src/lib/budget/budget.component.ts
+++ feature/budget/src/lib/budget/budget.component.ts
@@ -6,68 +6,75 @@ import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
 import { ButtonModule } from 'primeng/button';
-import { Checkbox } from 'primeng/checkbox';
-import { AccumulatedBudgetItem, BudgetItem } from './models';
-import * as dayjs from 'dayjs';
+import { RadioButton } from 'primeng/radiobutton';
+import { AccumulatedBudgetItem, BudgetItem, RECURRENCE } from './models';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'bgt-budget',
-  imports: [CommonModule, DatePicker, FormsModule, InputTextModule, FloatLabel, ButtonModule, Checkbox, Button],
+  imports: [CommonModule, DatePicker, FormsModule, InputTextModule, FloatLabel, ButtonModule, RadioButton, Button],
   templateUrl: './budget.component.html',
   styleUrl: './budget.component.scss',
 })
 export class BudgetComponent {
   amount = signal(0);
   description = signal('');
-  recurring = signal(false);
-  date = signal(dayjs.unix(0));
+  recurring = signal<RECURRENCE>(RECURRENCE.NONE);
+  date = signal(dayjs());
+  targetHeight = signal(200);
+  RECURRENCE = RECURRENCE;
+  scale = signal(500);
 
   budgetItems = signal<BudgetItem[]>([
     {
       id: '1234',
-      description: 'Groceries',
-      date: +dayjs.unix(0).startOf('day').subtract(6, 'day').toDate(),
-      recurring: false,
-      amount: 40,
+      description: 'Starting',
+      date: +dayjs('2025-06-09').toDate(),
+      recurring: RECURRENCE.NONE,
+      amount: 5459.91,
     },
     {
       id: '1235',
-      description: 'Groceries',
-      date: +dayjs.unix(0).startOf('day').subtract(4, 'day').toDate(),
-      recurring: false,
-      amount: 30,
+      description: 'Credit Card',
+      date: +dayjs('2025-06-11').toDate(),
+      recurring: RECURRENCE.NONE,
+      amount: -1000,
     },
     {
       id: '1236',
-      description: 'Groceries',
-      date: +dayjs.unix(0).startOf('day').subtract(3, 'day').toDate(),
-      recurring: false,
-      amount: 100,
+      description: 'Salary',
+      date: +dayjs('2025-06-12').toDate(),
+      recurring: RECURRENCE.BIWEEKLY,
+      amount: 3033.84,
     },
     {
       id: '1237',
-      description: 'Groceries',
-      date: +dayjs.unix(0).startOf('day').subtract(1, 'day').toDate(),
-      recurring: false,
-      amount: 100,
+      description: 'Mortgage',
+      date: +dayjs('2025-06-15').toDate(),
+      recurring: RECURRENCE.MONTHLY,
+      amount: -4100,
     },
     {
       id: '1238',
-      description: 'Phone',
-      date: +dayjs.unix(0).startOf('day').toDate(),
-      recurring: true,
-      amount: 10,
+      description: 'ATM',
+      date: +dayjs('2025-06-16').toDate(),
+      recurring: RECURRENCE.NONE,
+      amount: -100,
     },
     {
-      id: '1239',
-      description: 'Mortgage',
-      date: +dayjs.unix(0).startOf('day').toDate(),
-      recurring: true,
-      amount: 1500,
+      id: '1236',
+      description: 'Salary',
+      date: +dayjs('2025-06-26').toDate(),
+      recurring: RECURRENCE.BIWEEKLY,
+      amount: 3033.84,
     },
   ]);
+
+  emptyItems = signal<BudgetItem[]>(this.generateEmpties());
+
   processedItems = computed<AccumulatedBudgetItem[]>(() => {
-    const sorted = this.budgetItems().sort((a, b) => a.date - b.date);
+    const empties = this.emptyItems();
+    const sorted = [...this.budgetItems(), ...empties].sort((a, b) => a.date - b.date);
     let total = 0;
     return sorted.map((item) => {
       total += item.amount;
@@ -78,8 +85,12 @@ export class BudgetComponent {
     });
   });
 
+  /**
+   * Group items by day, with the chang
+   */
   groupedItems = computed<AccumulatedBudgetItem[][]>(() => {
     const items = this.processedItems();
+
     const groups: AccumulatedBudgetItem[][] = [];
     let currentGroup: AccumulatedBudgetItem[] = [];
     items.forEach((item) => {
@@ -91,6 +102,33 @@ export class BudgetComponent {
     });
 
     return groups;
+  });
+
+  groupTotals = computed(() => {
+    const totals = this.groupedItems().map((group) => {
+      return group.reduce((total, item) => {
+        let most = 0;
+        if (item.total > most) {
+          most = item.total;
+        }
+        return {
+          ...total,
+          date: item.date,
+          total: most,
+        };
+      });
+    });
+    return totals;
+  });
+
+  tallest = computed(() => {
+    let tallest = 0;
+    this.groupTotals().forEach((group) => {
+      if (group.total > tallest) {
+        tallest = group.total;
+      }
+    });
+    return tallest;
   });
 
   addItem() {
@@ -110,6 +148,31 @@ export class BudgetComponent {
     this.budgetItems.set([...this.budgetItems().filter((i) => i.id !== id)]);
   }
 
+  recurr(item: BudgetItem) {
+    this.budgetItems.update((items) => [
+      ...items,
+      {
+        ...item,
+        date: +dayjs(item.date).add(this.getRecurrence(item), 'day').toDate(),
+      },
+    ]);
+  }
+
+  getRecurrence(item: BudgetItem) {
+    switch (item.recurring) {
+      case RECURRENCE.WEEKLY:
+        return 7;
+      case RECURRENCE.BIWEEKLY:
+        return 14;
+      case RECURRENCE.MONTHLY:
+        return 30;
+      case RECURRENCE.YEARLY:
+        return 365;
+      default:
+        return 1;
+    }
+  }
+
   getRandomHexColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -121,10 +184,24 @@ export class BudgetComponent {
 
   barStyle(height: number) {
     return {
-      height: `${height / 10}px`,
+      height: `${(height / this.tallest()) * this.targetHeight()}px`,
       display: 'inline-block',
       width: '20px',
       background: this.getRandomHexColor(),
     };
+  }
+
+  generateEmpties(month = 0) {
+    return (
+      new Array(60)
+        // return new Array(dayjs().daysInMonth())
+        .fill({
+          id: '-1',
+          description: 'Label',
+          date: +dayjs().startOf('day').toDate(),
+          amount: 0,
+        })
+        .map((item, i) => ({ ...item, date: +dayjs().startOf('month').add(month, 'month').add(i, 'day').toDate() }))
+    );
   }
 }
--- feature/budget/src/lib/budget/models.ts
+++ feature/budget/src/lib/budget/models.ts
@@ -3,9 +3,18 @@ export interface BudgetItem {
   description: string;
   date: number;
   amount: number;
-  recurring: boolean;
+  recurring: RECURRENCE;
 }
 
 export interface AccumulatedBudgetItem extends BudgetItem {
   total: number;
+}
+
+export enum RECURRENCE {
+  NONE = 'none',
+  DAILY = 'daily',
+  WEEKLY = 'weekly',
+  BIWEEKLY = 'bi-weekly',
+  MONTHLY = 'monthly',
+  YEARLY = 'yearly',
 }
