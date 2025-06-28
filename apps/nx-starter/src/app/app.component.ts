12575b3158514e7a4d0b31b5ff94a034b69e7b3b
artesgo <artesgo@gmail.com>
2025-06-28 15:30:20 -0400

    chore: remove testing code

--- apps/nx-starter/src/app/app.component.ts
+++ apps/nx-starter/src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
-import { HttpClient } from '@angular/common/http';
-import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   imports: [RouterModule, NavComponent],
@@ -10,16 +8,4 @@ import { toSignal } from '@angular/core/rxjs-interop';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'nx-starter';
-
-  http = inject(HttpClient);
-
-  toast = this.http.get('http://localhost:4200/api/v1/toaster', {
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  $ = {
-    toast: toSignal(this.toast),
-  };
-}
+export class AppComponent {}
