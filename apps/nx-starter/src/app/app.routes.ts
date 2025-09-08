88009d5a589b617584789dab17e3cde78a1e9349
artesgo <artesgo@gmail.com>
2025-09-08 08:18:46 -0400

    fix: import export array

--- apps/nx-starter/src/app/app.routes.ts
+++ apps/nx-starter/src/app/app.routes.ts
@@ -1,18 +1,12 @@
 import { Route } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    redirectTo: 'budget',
-    pathMatch: 'full',
-  },
-  {
-    path: 'budget',
     loadChildren: () => import('@nx-starter/budget').then((m) => m.routes),
   },
   {
-    path: 'home',
-    component: HomeComponent,
+    path: '**',
+    redirectTo: '',
   },
 ];
